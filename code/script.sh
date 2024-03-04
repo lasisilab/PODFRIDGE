2e3be8153f7db0292008dac27be249a160763054
Tina Lasisi <tina.lasisi@gmail.com>
2024-03-03 20:01:48 -0500

    wflow_publish("analysis/*", republish = TRUE, all = TRUE, verbose = TRUE)

--- code/script.sh
+++ code/script.sh
@@ -1,15 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --job-name=known_vs_tested_simulation
-#SBATCH --output=/project/jazlynmo_738/Tina/PODFRIDGE_upload/logfiles/known_vs_tested_simulation_%j.out
-#SBATCH --error=/project/jazlynmo_738/Tina/PODFRIDGE_upload/logfiles/known_vs_tested_simulation_%j.err
+#SBATCH --output=/home/tlasisi/PODFRIDGE_upload/logfiles/known_vs_tested_simulation_%j.out
+#SBATCH --error=/home/tlasisi/PODFRIDGE_upload/logfiles/known_vs_tested_simulation_%j.err
 #SBATCH --time=16:00:00
-#SBATCH -p epyc-64
+#SBATCH --partition=standard
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=tlasisi@usc.edu
+#SBATCH --mail-user=tlasisi@umich.edu
 
-module load gcc/11.3.0 openblas/0.3.20 r/4.3.0
+# Load necessary modules
+module load gcc/9.3.0 openblas/0.3.13 r/4.0.5
 
-Rscript /project/jazlynmo_738/Tina/PODFRIDGE_upload/code/known-vs-tested_simulation_script.R 100000 5000
+# Ensure the directory structure exists
+mkdir -p /home/tlasisi/PODFRIDGE_upload/logfiles
+
+# Run the R script
+Rscript /home/tlasisi/PODFRIDGE_upload/code/known-vs-tested_simulation_script.R 100000 5000
