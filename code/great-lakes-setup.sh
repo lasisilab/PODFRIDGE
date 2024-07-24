4e6da590ac0ae1f54edf718faeacc1f96e9ad8a6
tinalasisi <tina.lasisi@gmail.com>
2024-07-24 19:57:01 -0400

    Update great-lakes-setup.sh

--- code/great-lakes-setup.sh
+++ code/great-lakes-setup.sh
@@ -1,12 +1,21 @@
+# Setting up account first time
+export UNIQNAME=tlasisi
+cd /nfs/turbo/lsa-tlasisi1
+mkdir $UNIQNAME
+ln -s /nfs/turbo/lsa-tlasisi1/$UNIQNAME /home/$UNIQNAME
+cd $UNIQNAME
+
 # Setting up simulation on Great Lakes
-#
-#
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # run the installer
-#
 bash Miniconda3-latest-Linux-x86_64.sh
 
+# activate conda
+source ~/.bashrc
+
+# verify installation
+conda --version
 
 # Ensure the R and Conda-Forge channels are added
 conda config --add channels r
