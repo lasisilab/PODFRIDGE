5ee8ac084891e83c61d853e93495010e24e11daa
tinalasisi <tina.lasisi@gmail.com>
2024-07-24 22:15:56 -0400

    Update script and delete files

--- code/great-lakes-setup.sh
+++ code/great-lakes-setup.sh
@@ -1,5 +1,9 @@
 # Setting up account first time
+# In the future start with exporting and go to ssh
 export UNIQNAME=tlasisi
+
+ssh $UNIQNAME@greatlakes.arc-ts.umich.edu
+
 cd /nfs/turbo/lsa-tlasisi1
 mkdir $UNIQNAME
 ln -s /nfs/turbo/lsa-tlasisi1/$UNIQNAME /home/$UNIQNAME
@@ -8,24 +12,33 @@ cd $UNIQNAME
 # Setting up simulation on Great Lakes
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
+#check install isn't corrputed
+shasum -a 256 Miniconda3-latest-Linux-x86_64.sh
+
+
 # run the installer
 bash Miniconda3-latest-Linux-x86_64.sh
 
-# activate conda
-source ~/.bashrc
 
-# verify installation
-conda --version
+#install miniconda here
+/home/$UNIQNAME/$UNIQNAME/miniconda3
+
+
+# Install mamba in the base conda environment with verbose output
+conda install -n base -c conda-forge mamba -y -v
+
+# Create a new environment named 'rstats' with verbose output
+mamba create -n rstats -c conda-forge r -y -v
+
 
 # Ensure the R and Conda-Forge channels are added
 conda config --add channels r
 conda config --add channels conda-forge
 
 # Create a new environment named 'rstats'
-conda create -n rstats -c r -c conda-forge r -y
+mamba create -n rstats -c r -c conda-forge r --verbose
 
 # Activate the environment
-source $(conda info --base)/etc/profile.d/conda.sh
 conda activate rstats
 
 # Read the STR-sims-env.txt file and install each package
--- code/script.sh
+++ code/script.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 #SBATCH --job-name=known_vs_tested_simulation
 #SBATCH --output=/home/%u/%u/slurm/%x-%j.log
-#SBATCH --time=14-00:00:000
+#SBATCH --time=14-00:00:00
 #SBATCH --account=tlasisi0
 #SBATCH --partition=standard
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=36
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jakevanc@umich.edu
+#SBATCH --mail-user=${USER}@umich.edu
 
-# stimulate conda
-CONDA_HOME="/home/jakevanc/miniconda3"
+# Get the unique username
+UNIQNAME=$USER
+
+# Activate conda
+CONDA_HOME="/home/$UNIQNAME/miniconda3"
 source "$CONDA_HOME/etc/profile.d/conda.sh" || exit 1
 
-# Load conda env created with conda env create -f env.yml
-conda activate simulations
+# Create the environment using the environment.yml file
+conda env create -f /path/to/environment.yml
+
+# Activate the environment
+conda activate rstats
 
 # Ensure the directory structure exists
-mkdir -p /home/jakevanc/jakevanc/PODFRIDGE/logfiles
+mkdir -p /home/$UNIQNAME/$UNIQNAME/PODFRIDGE/logfiles
 
 # Run the R script
-Rscript /home/jakevanc/jakevanc/PODFRIDGE/code/known-vs-tested_simulation_script.R 100000 50000
+Rscript /home/$UNIQNAME/$UNIQNAME/PODFRIDGE/code/known-vs-tested_simulation_script.R 100000 50000
+
