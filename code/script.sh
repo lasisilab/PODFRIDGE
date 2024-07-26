6bfd212a9abeb3866794ae1416d5f7e3d0d73710
Tina Lasisi <tina.lasisi@gmail.com>
2024-07-26 15:55:49 -0400

    Update script.sh

--- code/script.sh
+++ code/script.sh
@@ -9,6 +9,19 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=$(whoami)@umich.edu
 
+# Get the unique username
+UNIQNAME=$(whoami)
+
+# Create output folder with timestamp
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+OUTPUT_DIR="/home/$UNIQNAME/$UNIQNAME/PODFRIDGE/output/simulation_$TIMESTAMP"
+mkdir -p $OUTPUT_DIR/slurm
+mkdir -p $OUTPUT_DIR/logfiles
+
+# Set the directory for slurm output
+#SBATCH --output=$OUTPUT_DIR/slurm/%x-%j.log
+
+# Reminder to set GitHub PAT and username
 ########################################################################
 # Reminder: Please set your GitHub PAT using the following command     #
 # before running this script:                                          #
@@ -16,31 +29,19 @@
 # export GITHUB_USER='your_github_username'                            #
 ########################################################################
 
-# Get the unique username
-UNIQNAME=$(whoami)
-
-# Define the timestamp and create output directory for this job
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-OUTPUT_DIR="/home/$UNIQNAME/$UNIQNAME/PODFRIDGE/output/simulation_${TIMESTAMP}"
-
-# Ensure the directory structure exists
-mkdir -p $OUTPUT_DIR/slurm
-mkdir -p $OUTPUT_DIR/logfiles
-
-# Update SBATCH output to write to the newly created directory
-#SBATCH --output=$OUTPUT_DIR/slurm/%x-%j.log
-
 # Change to the project directory
 cd /home/$UNIQNAME/$UNIQNAME/PODFRIDGE
 
-# Activate conda environment
+# Activate conda
 CONDA_HOME="/home/$UNIQNAME/$UNIQNAME/miniconda3"
 source "$CONDA_HOME/etc/profile.d/conda.sh"
+
+# Activate the environment
 conda activate rstats
 
 # Log memory and CPU usage every 15 minutes
 log_resource_usage() {
-    squeue --job=$SLURM_JOB_ID --format="%.18i %.9P %.8j %.8u %.2t %.10M %.6D %R %C %m %N" | tee -a $OUTPUT_DIR/logfiles/resource_usage_$SLURM_JOB_ID.log
+    squeue --job=$SLURM_JOB_ID --format="%.18i %.9P %.8j %.8u %.2t %.10M %.6D %R %C %m %N" | tee -a $OUTPUT_DIR/slurm/resource_usage_$SLURM_JOB_ID.log
 }
 
 # Run the logging function every 15 minutes in the background
@@ -49,14 +50,16 @@ while true; do
     sleep 900  # Pause for 15 minutes
 done &
 
-# Run the R script with command line arguments for simulation counts and output directory
+# Run the R script with command line arguments
 Rscript code/STR_sims.R 5 10 $OUTPUT_DIR
+
+# Log memory and CPU usage after job completion
+sacct -j $SLURM_JOB_ID --format=JobID,JobName,Partition,AllocCPUs,Elapsed,MaxRSS,MaxVMSize,State > $OUTPUT_DIR/slurm/usage_$SLURM_JOB_ID.log
 
 # Configure Git to use HTTPS and PAT
 git remote set-url origin https://github.com/lasisilab/PODFRIDGE.git
+
+# Commit and push changes to GitHub
 git add .
 git commit -m "Automated commit of new results $(date)"
 echo $GITHUB_PAT | git push https://$GITHUB_USER:$GITHUB_PAT@github.com/lasisilab/PODFRIDGE.git
-
-# Log final memory and CPU usage after job completion
-sacct -j $SLURM_JOB_ID --format=JobID,JobName,Partition,AllocCPUs,Elapsed,MaxRSS,MaxVMSize,State > $OUTPUT_DIR/logfiles/usage_$SLURM_JOB_ID.log
