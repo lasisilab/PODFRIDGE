de8d1627f8ace277e883fd89393095cc9033f6fe
robinsonha <126197658+robinsonha@users.noreply.github.com>
2024-08-06 10:47:30 +0100

    Update simulate_genotypes.R
    Update cluster syntax and use of plan

--- code/simulate_genotypes.R
+++ code/simulate_genotypes.R
@@ -7,12 +7,29 @@ suppressMessages(suppressWarnings({
   library(parallel)
 }))
 
-# Set up cluster
-cl <- makeCluster(availableCores())
-plan(cluster, workers = cl)
+#Set options for run- this will ultimately be parameterised and sit in the primary run script
+#(The following lines to set variables are temporary for testing and will not remain in this script)
+use_remote_cluster<-1 #If sending to external cluster (use 0 for tests on one machine)
 
-# Stop cluster when the script exits
-on.exit(parallel::stopCluster(cl))
+# Set up cluster on one machine if required
+if(use_remote_cluster==0){
+  if( .Platform$OS.type == "windows" ){
+    cl <- makeCluster(availableCores())  #specify how many cores to use
+  } else { # use the fork cluster type on linux because its faster- not available for windows
+    cl <- makeCluster(availableCores(),type="FORK")  #specify how many cores to use
+  } 
+  # Ensure the cluster is stopped when the script exits
+  on.exit(parallel::stopCluster(cl))
+  plan(cluster, workers = cl)
+} else {
+  plan(cluster, workers = c("clustername1", "clustername2", "server.remote.org- if using an online cluster", "etc"))  
+}  
+
+#plan() options:
+#use 'multisession' to run in parallel in separate R sessions on the same machine
+#use 'multicore' to run futures in parallel in forked processes on the same machine- Linux only
+#use 'cluster' to run in parallel on one or more machines
+
 
 # Helper function for logging
 log_message <- function(message) {
