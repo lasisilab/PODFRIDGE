1db1aa601ff0acec436064c788b63980653a5965
robinsonha <126197658+robinsonha@users.noreply.github.com>
2024-08-26 10:14:48 +0100

    Update synthetic_data.R
    Update to unique marker names

--- code/synthetic_data.R
+++ code/synthetic_data.R
@@ -5,6 +5,18 @@ require(dplyr)
 
 df_allelefreq <- fread("data/df_allelefreq_combined.csv")
 df_allelefreq$population<-as.factor(df_allelefreq$population)
+df_allelefreq$marker<-as.factor(df_allelefreq$marker)
+
 syn_data<-df_allelefreq %>%
   group_by(population) %>%
   do(sample_n(., 1100, replace = TRUE))
+
+syn_data<-syn_data %>%
+  group_by(population,marker) %>%
+  mutate(COUNTER = 1:n()) %>%  
+  ungroup()
+syn_data$marker<-paste(syn_data$COUNTER,syn_data$marker,sep="_")
+syn_data<-syn_data %>%
+  select(-COUNTER)
+
+  
